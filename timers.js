661a034e4707e879ee9b0ad98b1ab8fdd26e5c79
David <david720@gmail.com>
2022-10-12 14:37:28 -0500

    Create timers.js

---
+++ timers.js
@@ -0,0 +1,26 @@
+function countDown(num) {
+    if (num === 0) {
+        console.log('DONE!');
+    } else {
+        console.log(num);
+        setTimeout (function () {
+            countDown(num - 1);
+        }, 1000)
+    }
+}
+
+// countDown(4);
+
+function randomGame() {
+    let count = 0;
+    const id = setInterval(function () {
+        count++;
+        let rand = Math.random();
+        if (rand > 0.75) {
+            clearInterval(id);
+            console.log(`It took ${count} attempts to get over 0.75`);
+        }
+    }, 1000);
+}
+
+// randomGame();
